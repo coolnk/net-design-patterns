b323cdf76df42b63c75ac5aa910b43d9ecf25d65
Nirjal Khadka <nirjal@gmail.com>
2017-05-06 23:51:55 +0200

    flyweight pattern

---
+++ flyweight-pattern/flyweight-pattern/ConcreteFlyweight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flyweight_pattern
+{
+    class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicState)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicState);
+        }
+    }
+}
---
+++ flyweight-pattern/flyweight-pattern/FlyweightFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flyweight_pattern
+{
+    class FlyweightFactory
+    {
+        private Hashtable _flyweights = new Hashtable();
+
+        public FlyweightFactory()
+        {
+            _flyweights.Add("X", new ConcreteFlyweight());
+            _flyweights.Add("Y", new ConcreteFlyweight());
+            _flyweights.Add("Z", new ConcreteFlyweight());
+        }
+
+        public Flyweight GetFlyweight(string key)
+        {
+            return((Flyweight) _flyweights[key]);        
+        }
+
+
+    }
+}
